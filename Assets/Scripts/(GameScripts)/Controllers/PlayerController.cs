3ebd274d24e37c6d3168d271b6ca4662a498a0e2
GracjanMWitos <gracjan-witos@wp.pl>
2021-01-31 21:13:04 +0100

    Laboratory location rooms plan finished. LWRP system added. Before unity version change.

--- Assets/Scripts/(GameScripts)/Controllers/PlayerController.cs
+++ Assets/Scripts/(GameScripts)/Controllers/PlayerController.cs
@@ -54,7 +54,6 @@ public class PlayerController : MonoBehaviour
         Quaternion rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward) * Quaternion.Euler(0, 0, 90), 10*Time.deltaTime);
         transform.rotation = rotation;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
-        Debug.Log(transform.eulerAngles);
         movement.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         movement.y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
